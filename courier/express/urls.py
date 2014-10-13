ef29210802e62d71bca49e616ebb944d2f62d48c
BridgeGeorge <1650730996@qq.com>
2014-10-13 20:41:46 +0800

    last commit

--- courier/express/urls.py
+++ courier/express/urls.py
@@ -2,6 +2,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('express.views',
-	url(r'^recevie/$', 'recevie', {'template_name':'recevie.html'}),    # 我要收件
+	url(r'^receive/$', 'receive', {'template_name':'receive.html'}),    # 我要收件
 	url(r'^send/$', 'send', {'template_name':'send.html'}),    # 我要发件
 )
